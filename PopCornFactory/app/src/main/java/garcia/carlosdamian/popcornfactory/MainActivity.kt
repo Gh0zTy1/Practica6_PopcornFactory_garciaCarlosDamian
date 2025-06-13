08b919f298b577e2151f5e8115e070de8b541620
Gh0zTy1 <160164643+Gh0zTy1@users.noreply.github.com>
2025-06-13 15:35:08 -0700

    Listop

---
+++ PopCornFactory/app/src/main/java/garcia/carlosdamian/popcornfactory/MainActivity.kt
@@ -0,0 +1,26 @@
+package garcia.carlosdamian.popcornfactory
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import garcia.carlosdamian.popcornfactory.R
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val buttonStart: Button = findViewById(R.id.button_get_started)
+
+        buttonStart.setOnClickListener {
+            var intent: Intent = Intent(this, Catalogo::class.java)
+            startActivity(intent)
+        }
+    }
+}
